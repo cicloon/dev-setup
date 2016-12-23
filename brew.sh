6b38ce399bf3ca473655e4f395cbfd3253e6d5fb
cicloon <aleon.prof@gmail.com>
2016-12-23 18:41:56 +0100

    Comment rbenv stuff

--- brew.sh
+++ brew.sh
@@ -57,10 +57,10 @@ brew install python
 brew install python3
 
 # Install ruby-build and rbenv
-brew install ruby-build
+# brew install ruby-build
 # brew install rbenv
-LINE='eval "$(rbenv init -)"'
-grep -q "$LINE" ~/.extra || echo "$LINE" >> ~/.extra
+# LINE='eval "$(rbenv init -)"'
+# grep -q "$LINE" ~/.extra || echo "$LINE" >> ~/.extra
 
 # Install Elixir and Phoenix
 brew install elixir
