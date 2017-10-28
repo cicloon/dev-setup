0f301b1a051ded870f3ce1f33bc16136dc923d15
cicloon <aleon.prof@gmail.com>
2017-10-28 12:25:23 +0200

    Skip ruby in the prompt, use node instead

--- .git-prompt-colors.sh
+++ .git-prompt-colors.sh
@@ -8,13 +8,7 @@
 # added ruby prompt, and kernel version as well
 override_git_prompt_colors() {
   GIT_PROMPT_THEME_NAME="Custom"
-  if [ -e ~/.rvm/bin/rvm-prompt ]; then
-    RUBY_PROMPT='{$(~/.rvm/bin/rvm-prompt i v)}'
-  else
-    if command -v rbenv > /dev/null; then
-      RUBY_PROMPT='{$(rbenv version | sed -e "s/ (set.*$//")}'
-    fi
-  fi
+  NODE_PROMPT='node {$(node -v)}'
   Time12a="\$(date +%H:%M:%S)"
 
   GIT_PROMPT_STAGED="${Yellow}● "
@@ -24,7 +18,7 @@ override_git_prompt_colors() {
   GIT_PROMPT_COMMAND_OK="${Green}✔ "
   GIT_PROMPT_COMMAND_FAIL="${Red}✘ "
 
-  GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_ ${BoldBlueFg}${Time12a} ${Cyan}${RUBY_PROMPT} ${Yellow}${PathShort}"
+  GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_ ${BoldBlueFg}${Time12a} ${Cyan}${NODE_PROMPT} ${Yellow}${PathShort}"
   GIT_PROMPT_START_ROOT="${GIT_PROMPT_START_USER}"
   if [ -n "$SSH_CLIENT" ]; then
     GIT_PROMPT_END_USER="\n${BoldRed} ➤ ${ResetColor}"
