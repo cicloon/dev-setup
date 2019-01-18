193c6ac0e423e69780b5a31fbbcecb93761b7f3c
cicloon <aleon.prof@gmail.com>
2019-01-18 19:31:58 +0100

    tweak app store applications

--- appstore.sh
+++ appstore.sh
@@ -19,19 +19,20 @@ brew upgrade --all
 
 brew install mas
 
-mas install 497799835 # Xcode (8.2)
-mas install 409789998 # Twitter (4.2.4)
-mas install 405580712 # StuffIt Expander (15.0.7)
-mas install 408981434 # iMovie (10.1.4)
-mas install 409183694 # Keynote (7.0.5)
-mas install 784801555 # Microsoft OneNote (15.29)
-mas install 918858936 # Airmail 3 (3.2.1)
-mas install 409201541 # Pages (6.0.5)
-mas install 682658836 # GarageBand (10.1.4)
-mas install 413857545 # Divvy (1.5)
-mas install 409203825 # Numbers (4.0.5)
-mas install 683129651 # LoremBuilder (1.2.1)
-mas install 946798523 # AntiSleep (1.2)
+mas install 497799835  # Xcode (10.1)
+mas install 405580712  # StuffIt Expander (15.0.7)
+mas install 409183694  # Keynote (8.3)
+mas install 784801555  # Microsoft OneNote (15.29)
+mas install 918858936  # Airmail 3 (3.2.1)
+mas install 409201541  # Pages (6.0.5)
+mas install 682658836  # GarageBand (10.1.4)
+mas install 413857545  # Divvy (1.5)
+mas install 409203825  # Numbers (5.3)
+mas install 784801555  # Microsoft OneNote
+mas install 1384080005 # Tweetbot (3.2)
+mas install 973134470  # Be Focused (1.7.5)
+mas install 946798523  # AntiSleep (2.13)
+
 
 killall "divvy" &> /dev/null
 sudo cp com.mizage.Divvy.plist ~/Library/Preferences/com.mizage.Divvy.plist
