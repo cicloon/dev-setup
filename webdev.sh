7d9b44d1c3cc5f18d24b499536648748406571fe
cicloon <aleon.prof@gmail.com>
2019-01-19 12:59:34 +0100

    fixing nvm installation

--- webdev.sh
+++ webdev.sh
@@ -20,6 +20,8 @@ brew update
 
 # Install nvm & node version
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+chmod 755 "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
